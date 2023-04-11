200c91760dff534f43422d65a784af27d74d376f
Pierre-Olivier Vauboin <po@lambdaconcept.com>
2023-04-11 16:23:10 +0200

    init repo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lambdalib",
+    author="Pierre-Olivier Vauboin",
+    author_email="po@lambdaconcept.com",
+    packages=find_packages(),
+    project_urls={
+        "Source Code": "https://github.com/lambdaconcept/lambdalib",
+        "Bug Tracker": "https://github.com/lambdaconcept/lambdalib/issues",
+    },
+)
