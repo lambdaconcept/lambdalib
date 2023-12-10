c69ae5f00e54c1155f643d84a535c865ba745d1c
key2 <keytwo@gmail.com>
2023-12-10 19:17:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="lambdalib",
-    author="Pierre-Olivier Vauboin",
-    author_email="po@lambdaconcept.com",
-    packages=find_packages(),
-    project_urls={
-        "Source Code": "https://github.com/lambdaconcept/lambdalib",
-        "Bug Tracker": "https://github.com/lambdaconcept/lambdalib/issues",
-    },
-)
+
+setup()
